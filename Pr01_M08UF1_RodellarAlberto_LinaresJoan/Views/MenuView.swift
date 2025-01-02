06f992f8699f8a4fb1e1c70e34201fcb6b67bea2
JoanLinares <joan.linares.vigo@gmail.com>
2025-01-02 13:39:19 +0100

    feat: the view of the 5 rarest cards

--- Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/MenuView.swift
+++ Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/MenuView.swift
@@ -1,8 +1,51 @@
-//
-//  MenuView.swift
-//  Pr01_M08UF1_RodellarAlberto_LinaresJoan
-//
-//  Created by Joan Linares Vigo on 31/12/24.
-//
+import SwiftUI
 
-import Foundation
+struct MenuView: View {
+    @StateObject private var viewModel = MenuViewModel()
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Cartas Más Caras")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.bottom, 10)
+
+                    ForEach(viewModel.expensiveCards, id: \.id) { card in
+                        HStack {
+                            if let url = URL(string: card.images.large) {
+                                RemoteImage(url: url)
+                                    .frame(width: 100, height: 140)
+                                    .cornerRadius(8)
+                                    .padding(.trailing, 10)
+                            }
+
+                            VStack(alignment: .leading) {
+                                Text(card.name)
+                                    .font(.headline)
+                                Text("Precio: $\(card.cardmarket?.prices.trendPrice ?? 0, specifier: "%.2f")")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                        .shadow(radius: 2)
+                        .padding(.horizontal)
+                    }
+                }
+                .padding()
+            }
+            .onAppear {
+                viewModel.loadExpensiveCards()
+            }
+            .navigationTitle("Cartas Pokémon")
+            .alert(item: $viewModel.error) { error in
+                Alert(title: Text("Error"), message: Text(error.message), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
