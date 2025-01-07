2b18bc035251f8df4d6bab3844c49f85b169b27a
JoanLinares <joan.linares.vigo@gmail.com>
2025-01-07 18:57:46 +0100

    Now runs the tab bar view

--- Pr01_M08UF1_RodellarAlberto_LinaresJoan/Pr01_M08UF1_RodellarAlberto_LinaresJoanApp.swift
+++ Pr01_M08UF1_RodellarAlberto_LinaresJoan/Pr01_M08UF1_RodellarAlberto_LinaresJoanApp.swift
@@ -1,17 +1,10 @@
-//
-//  Pr01_M08UF1_RodellarAlberto_LinaresJoanApp.swift
-//  Pr01_M08UF1_RodellarAlberto_LinaresJoan
-//
-//  Created by Joan Linares Vigo on 31/12/24.
-//
-
 import SwiftUI
 
 @main
 struct Pr01_M08UF1_RodellarAlberto_LinaresJoanApp: App {
     var body: some Scene {
         WindowGroup {
-            MenuView()
+            MainTabBarView()
         }
     }
 }
