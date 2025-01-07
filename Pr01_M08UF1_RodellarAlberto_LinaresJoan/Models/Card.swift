28748935b9b237e5468edd84e681f09bd7d163fb
JoanLinares <joan.linares.vigo@gmail.com>
2025-01-07 18:57:21 +0100

    Editing structure for Card to get the card number ad card id

--- Pr01_M08UF1_RodellarAlberto_LinaresJoan/Models/Card.swift
+++ Pr01_M08UF1_RodellarAlberto_LinaresJoan/Models/Card.swift
@@ -1,8 +1,29 @@
-//
-//  Card.swift
-//  Pr01_M08UF1_RodellarAlberto_LinaresJoan
-//
-//  Created by Joan Linares Vigo on 31/12/24.
-//
-
 import Foundation
+
+// Modelo para una carta Pokémon
+struct Card: Codable, Identifiable {
+    let id: String
+    let name: String
+    let images: CardImages
+    let cardmarket: CardMarket?
+    var setID: String? // Propiedad mutable para guardar el setID
+    let number: String // Número de la carta
+}
+
+// Modelo para las imágenes de la carta
+struct CardImages: Codable {
+    let small: String
+    let large: String
+}
+
+// Modelo para el mercado de cartas
+struct CardMarket: Codable {
+    let prices: MarketPrices
+}
+
+// Precios del mercado
+struct MarketPrices: Codable {
+    let averageSellPrice: Double?
+    let lowPrice: Double?
+    let trendPrice: Double?
+}
