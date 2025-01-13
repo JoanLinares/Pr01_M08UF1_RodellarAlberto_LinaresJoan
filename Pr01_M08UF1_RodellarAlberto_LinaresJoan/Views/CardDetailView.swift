e792d325cf066ed70e2eeb45c9c03ff66bb41e40
JoanLinares <joan.linares.vigo@gmail.com>
2025-01-13 17:00:54 +0100

    now works well

--- Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/CardDetailView.swift
+++ Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/CardDetailView.swift
@@ -4,24 +4,30 @@ struct CardDetailView: View {
     let card: Card
 
     var body: some View {
-        VStack(spacing: 20) {
-            if let url = URL(string: card.images.large) {
-                RemoteImage(url: url)
-                    .frame(width: 300, height: 420)
-                    .cornerRadius(12)
-            }
-            Text(card.name)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.top)
+        ScrollView {
+            VStack(spacing: 20) {
+                if let url = URL(string: card.images.large) {
+                    RemoteImage(url: url)
+                        .frame(width: 300, height: 420)
+                        .cornerRadius(12)
+                }
 
-            if let price = card.cardmarket?.prices.trendPrice {
-                Text("Price: $\(price, specifier: "%.2f")")
-                    .font(.headline)
-            }
+                Text(card.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.top)
 
-            Spacer()
+                if let price = card.cardmarket?.prices.trendPrice {
+                    Text("Price: $\(price, specifier: "%.2f")")
+                        .font(.headline)
+                }
+
+                // Puedes agregar más detalles sobre la carta aquí si es necesario
+                Spacer()
+            }
+            .padding()
+            .navigationTitle(card.name) // Título jerárquico
+            .navigationBarTitleDisplayMode(.inline) // Título en formato "inline"
         }
-        .padding()
     }
 }
