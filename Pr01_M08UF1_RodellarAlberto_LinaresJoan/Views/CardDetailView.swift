0e6c70f7741f0e5a58047ea8e9a78c329b8f6542
JoanLinares <joan.linares.vigo@gmail.com>
2025-01-20 16:17:34 +0100

    completed

--- Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/CardDetailView.swift
+++ Pr01_M08UF1_RodellarAlberto_LinaresJoan/Views/CardDetailView.swift
@@ -4,30 +4,36 @@ struct CardDetailView: View {
     let card: Card
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                if let url = URL(string: card.images.large) {
-                    RemoteImage(url: url)
-                        .frame(width: 300, height: 420)
-                        .cornerRadius(12)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(spacing: 20) {
+                    if let url = URL(string: card.images.large) {
+                        RemoteImage(url: url)
+                            .frame(
+                                width: geometry.size.width > geometry.size.height ? 200 : 300, // Más pequeño en horizontal
+                                height: geometry.size.width > geometry.size.height ? 280 : 420 // Proporcional en horizontal
+                            )
+                            .cornerRadius(12)
+                            .frame(maxWidth: .infinity) // Centrar horizontalmente
+                    }
+
+                    Text(card.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.top)
+
+                    if let price = card.cardmarket?.prices.trendPrice {
+                        Text("Price: $\(price, specifier: "%.2f")")
+                            .font(.headline)
+                    }
+
+                    Spacer()
                 }
-
-                Text(card.name)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top)
-
-                if let price = card.cardmarket?.prices.trendPrice {
-                    Text("Price: $\(price, specifier: "%.2f")")
-                        .font(.headline)
-                }
-
-                // Puedes agregar más detalles sobre la carta aquí si es necesario
-                Spacer()
+                .padding()
+                .frame(maxWidth: .infinity) // Asegurar que todo el contenido esté centrado horizontalmente
+                .navigationTitle(card.name) // Título jerárquico
+                .navigationBarTitleDisplayMode(.inline) // Título en formato "inline"
             }
-            .padding()
-            .navigationTitle(card.name) // Título jerárquico
-            .navigationBarTitleDisplayMode(.inline) // Título en formato "inline"
         }
     }
 }
